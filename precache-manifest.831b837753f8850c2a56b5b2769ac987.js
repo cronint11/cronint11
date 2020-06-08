14372c4d84c278dd29e70d8931d085274bf50c05
Six4 <tm.timcronin@gmail.com>
2020-06-08 19:47:37 -0400

    Updates

---
+++ precache-manifest.831b837753f8850c2a56b5b2769ac987.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "882b7143e83f842742cf1774ed4bdccc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a06798f1b380aa3375d",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "40a35e5c9b45013a2ca6",
+    "url": "/static/css/main.ed29836a.chunk.css"
+  },
+  {
+    "revision": "3a06798f1b380aa3375d",
+    "url": "/static/js/2.083269dc.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.083269dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40a35e5c9b45013a2ca6",
+    "url": "/static/js/main.a79655d4.chunk.js"
+  },
+  {
+    "revision": "ef7fa6270e44380055dd",
+    "url": "/static/js/runtime-main.811639d9.js"
+  },
+  {
+    "revision": "879760d3b500321cade0f3ace2d9c659",
+    "url": "/static/media/Cane-wide.879760d3.jpg"
+  },
+  {
+    "revision": "ad0a0704c643ad2be64b73caef137c59",
+    "url": "/static/media/Jeffe-wide.ad0a0704.jpg"
+  },
+  {
+    "revision": "fb15958a603505c145f03acce6c771fc",
+    "url": "/static/media/Jon Dough-wide.fb15958a.jpg"
+  },
+  {
+    "revision": "51fd37208a8c9cfd835f5fb071888f83",
+    "url": "/static/media/csharp.51fd3720.png"
+  },
+  {
+    "revision": "e0c3329150018e3cbfa748e3b0f0a9ec",
+    "url": "/static/media/frisbee.e0c33291.gif"
+  },
+  {
+    "revision": "f21dceb6e5a41633aeb6284a656f0bde",
+    "url": "/static/media/mac.f21dceb6.png"
+  },
+  {
+    "revision": "8304b59f1d1c30f1db26e11e6ab783f2",
+    "url": "/static/media/nosql.8304b59f.png"
+  },
+  {
+    "revision": "decf9b0d8c7b4d1321d3088eafb93db8",
+    "url": "/static/media/profile_pic.decf9b0d.jpg"
+  }
+]);
